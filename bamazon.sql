b42ad5515039623a5ce9c586f427db6d5b46b336
Michael Pohlman <michael.pohlman1@gmail.com>
2018-12-14 01:31:00 -0500

    customer working with great table display

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+	item_id AUTO_INCREMENT NOT NULL,
+	product_name TEXT NOT NULL,
+	department_name TEXT NOT NULL,
+	price DECIMAL(2) NOT NULL,
+	stock_quantity INT NOT NULL,
+	PRIMARY KEY (item_id)
+);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Red Bull', 'Grocery', 2.99, 750),
+('Balance Protine Bars 12 pack', 'Grocery', 10.99, 106),
+('Red Rocket Dog Collar', 'Pet Supplies', 11.99, 150),
+('iPad Pro', 'Electronics', 609.99, 221),
+('Samsung 70in 4k LED TV', 'Electronics', 799.99, 100),
+('Mountain Bike', 'Sports & Outdoors', 435.99, 30),
+('Warrior Lacrosse Stick', 'Sports & Outdoors', 99.99, 50),
+('Bounty Paper Towels 36 pack', 'Bamazon Basics', 24.99, 400),
+('Stainless Steal 36 peice set', 'Bamazon Necessities', 37.99, 225),
+('Executive Office Chair', 'Office Products', 209.99, 21);
---
+++ bamazonCustomer.js
@@ -0,0 +1,114 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  // put in your user ID here
+  user: "",
+  // put in your password here
+  password: "",
+  database: "bamazon_db"
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Thank you for chosing to shop at Bamazon, ');
+  console.log('we have awesome products avalible for you today')
+  // confirm myqsl is connected and display inventory
+  displayProducts();
+});
+
+var displayProducts = function () {
+  connection.query(`SELECT * FROM products`, (err, res) => {
+    var listTable = new Table({
+      head: ['Item ID', 'Product Name', 'Price'],
+      colWidths: [10, 45, 12]
+    });
+
+    for (var i = 0; i < res.length; i++) {
+      listTable.push([res[i].item_id, res[i].product_name, `$${res[i].price}`]);
+    }
+
+    console.log(`\n\n${listTable.toString()}\n\n`);
+    promptUserPurchase();
+  });
+};
+
+function validateInput(value) {
+  var integer = Number.isInteger(parseFloat(value));
+  var numcheck = Math.sign(value);
+
+  if (integer && (numcheck === 1)) {
+    return true;
+  } else {
+    return 'Please enter a whole non-zero number.';
+  }
+}
+
+// ask user which product they would like to purchase
+function promptUserPurchase() {
+
+  // Prompt the user to select an item ID
+  inquirer.prompt([{
+      type: 'input',
+      name: 'item_id',
+      message: 'Please enter the Item ID which you would like to purchase today.',
+      validate: validateInput,
+      filter: Number
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: 'How many would you like to purchase?',
+      validate: validateInput,
+      filter: Number
+    }
+  ]).then((input) => {
+    var product = input.item_id;
+    var quantity = input.quantity;
+
+    // Query db to confirm that the given item ID exists in the desired quantity
+    var queryStr = 'SELECT * FROM products WHERE ?';
+
+    connection.query(queryStr, {
+      item_id: product
+    }, function (err, res) {
+      if (err) throw err;
+
+      if (res.length === 0) {
+        console.log('ERROR: Please select a valid Item ID.');
+        displayItems();
+
+      } else {
+        var productData = res[0];
+        // If have quantity requested process transaction
+        if (quantity <= productData.stock_quantity) {
+          console.log('Success!!! We are able to fulfill your order today');
+
+          // Construct the updating query string
+          var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + product;
+
+          // Update the inventory and inform customer of their bill of sale
+          connection.query(updateQueryStr, function (err, res) {
+            if (err) throw err;
+
+            console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+            console.log('Thank you for shopping with us today!');
+            console.log("\n---------------------------------------------------------------------\n");
+
+            // End the database connection
+            connection.end();
+          })
+        } else {
+          console.log('Sorry, there is not enough product in stock, ');
+          console.log('we are unable to process this large of an order at this time.');
+          console.log('We appoligize for the inconvience this may cause.')
+          console.log("\n---------------------------------------------------------------------\n");
+
+          displayProducts();
+        }
+      }
+    })
+  })
+}
